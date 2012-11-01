789c2350bf2236114b1ae780dabcd662c93d57ab
Jens Mikkelsen <jbmikk@gmail.com>
2012-11-01 16:05:46 -0300

    Add realloc and renew functions

--- clib/cmemory.c
+++ clib/cmemory.c
@@ -13,6 +13,18 @@ cpointer c_malloc_n(csize n)
     return ptr;
 }
 
+cpointer c_realloc_n(cpointer ptr, csize n)
+{
+    if(n > 0)
+    {
+        ptr = (cpointer) realloc(ptr, n);
+        if (ptr == NULL)
+            exit(EXIT_FAILURE);
+    }
+    else ptr = NULL;
+    return ptr;
+}
+
 void c_free(cpointer ptr)
 {
     free(ptr);
--- clib/cmemory.h
+++ clib/cmemory.h
@@ -5,9 +5,11 @@
 #include "ctypes.h"
 
 #define c_new(T, N) (T*)c_malloc_n(((csize)sizeof(T))*(N));
+#define c_renew(P, T, N) (T*)c_realloc_n(P, ((csize)sizeof(T))*(N));
 #define c_delete(P) c_free((cpointer)P)
 
 cpointer c_malloc_n(csize n);
+cpointer c_realloc_n(cpointer ptr, csize n);
 void c_free(cpointer ptr);
 
 #endif // CMEMORY_H
