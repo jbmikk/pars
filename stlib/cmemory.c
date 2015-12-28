681240e12910788c79d2b334f87b050af9272aa1
Jens Mikkelsen <jbmikk@gmail.com>
2015-12-27 23:36:55 -0300

    Fix cmemory code style

--- stlib/cmemory.c
+++ stlib/cmemory.c
@@ -3,25 +3,25 @@
 void *c_malloc_n(unsigned int n)
 {
 	void *ptr;
-	if(n > 0)
-	{
+	if(n > 0) {
 		ptr = (void *) malloc(n);
 		if (ptr == NULL)
 			exit(EXIT_FAILURE);
+	} else {
+		ptr = NULL;
 	}
-	else ptr = NULL;
 	return ptr;
 }
 
 void *c_realloc_n(void *ptr, unsigned int n)
 {
-	if(n > 0)
-	{
+	if(n > 0) {
 		ptr = (void *) realloc(ptr, n);
 		if (ptr == NULL)
 			exit(EXIT_FAILURE);
+	} else {
+		ptr = NULL;
 	}
-	else ptr = NULL;
 	return ptr;
 }
 
