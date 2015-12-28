51156869f86cec25644d46d1a8bb5bbcb2f72936
Jens Mikkelsen <jbmikk@gmail.com>
2015-12-27 23:31:37 -0300

    Fix cmemory indentation

--- stlib/cmemory.c
+++ stlib/cmemory.c
@@ -2,31 +2,31 @@
 
 void *c_malloc_n(unsigned int n)
 {
-    void *ptr;
-    if(n > 0)
-    {
-        ptr = (void *) malloc(n);
-        if (ptr == NULL)
-            exit(EXIT_FAILURE);
-    }
-    else ptr = NULL;
-    return ptr;
+	void *ptr;
+	if(n > 0)
+	{
+		ptr = (void *) malloc(n);
+		if (ptr == NULL)
+			exit(EXIT_FAILURE);
+	}
+	else ptr = NULL;
+	return ptr;
 }
 
 void *c_realloc_n(void *ptr, unsigned int n)
 {
-    if(n > 0)
-    {
-        ptr = (void *) realloc(ptr, n);
-        if (ptr == NULL)
-            exit(EXIT_FAILURE);
-    }
-    else ptr = NULL;
-    return ptr;
+	if(n > 0)
+	{
+		ptr = (void *) realloc(ptr, n);
+		if (ptr == NULL)
+			exit(EXIT_FAILURE);
+	}
+	else ptr = NULL;
+	return ptr;
 }
 
 void c_free(void *ptr)
 {
-    free(ptr);
+	free(ptr);
 }
 
