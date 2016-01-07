29a6fa3068ec8d1b7f599d7775971f080542785b
Jens Mikkelsen <jbmikk@gmail.com>
2016-01-07 00:14:28 -0300

    Fix test input indentation

--- testpars/test_input.c
+++ testpars/test_input.c
@@ -14,19 +14,19 @@ void teardown(Fixture *fix, gconstpointer data){
 }
 
 void input_init__existent_file(Fixture *fix, gconstpointer data){
-    Input *input = input_init("grammars/test_ebnf_grammar.txt");
-    g_assert(input != NULL);
-    input_close(input);
+	Input *input = input_init("grammars/test_ebnf_grammar.txt");
+	g_assert(input != NULL);
+	input_close(input);
 }
 
 void input_init__missing_file(Fixture *fix, gconstpointer data){
-    Input *input = input_init("should_not_exist.txt");
-    g_assert(input == NULL);
+	Input *input = input_init("should_not_exist.txt");
+	g_assert(input == NULL);
 }
 
 int main(int argc, char** argv){
-    g_test_init(&argc, &argv, NULL);
-    g_test_add("/Input/input_init", Fixture, NULL, setup, input_init__existent_file, teardown);
-    g_test_add("/Input/input_init", Fixture, NULL, setup, input_init__missing_file, teardown);
-    return g_test_run();
+	g_test_init(&argc, &argv, NULL);
+	g_test_add("/Input/input_init", Fixture, NULL, setup, input_init__existent_file, teardown);
+	g_test_add("/Input/input_init", Fixture, NULL, setup, input_init__missing_file, teardown);
+	return g_test_run();
 }
