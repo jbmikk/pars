6079fd368b97c6f8411a9abd9f8c63906c3b1491
Jens Mikkelsen <jbmikk@gmail.com>
2016-01-21 23:04:29 -0300

    Fix code style in testpars

--- testpars/testpars.c
+++ testpars/testpars.c
@@ -4,7 +4,7 @@
 #include "pars.h"
 
 typedef struct {
-    int placeholder;
+	int placeholder;
 } Fixture;
 
 void pars_setup(Fixture *fix, gconstpointer data){
@@ -14,12 +14,12 @@ void pars_teardown(Fixture *fix, gconstpointer data){
 }
 
 void test_load_grammar(Fixture *fix, gconstpointer data){
-    pars_load_grammar(NULL);
-    g_assert_cmpstr("bla", ==, "bla");
+	pars_load_grammar(NULL);
+	g_assert_cmpstr("bla", ==, "bla");
 }
 
 int main(int argc, char** argv){
-    g_test_init(&argc, &argv, NULL);
-    g_test_add("/Pars/pars_load_grammar", Fixture, NULL, pars_setup, test_load_grammar, pars_teardown);
-    return g_test_run();
+	g_test_init(&argc, &argv, NULL);
+	g_test_add("/Pars/pars_load_grammar", Fixture, NULL, pars_setup, test_load_grammar, pars_teardown);
+	return g_test_run();
 }
