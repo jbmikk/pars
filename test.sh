c207462a6426fd3aca3b14e93b8ffbd89c7bcbe2
Jens Mikkelsen <jbmikk@gmail.com>
2016-01-23 15:06:44 -0300

    Rewrite test scripts

--- test.sh
+++ test.sh
@@ -1,4 +1,31 @@
-sh build.sh OFF
+#setup $PWD variable
+cd -P .
+
+ROOT=$PWD
+
+run_test()
+{
+	VALOPTIONS="--suppressions=$ROOT/suppressions.supp --leak-check=full --show-leak-kinds=all --track-origins=yes"
+
+	if [ "$1" = "leaks" ]; then
+		valgrind $VALOPTIONS $2
+	fi
+
+	if [ "$1" = "run" -o "$1" = "trace" ]; then
+		$2
+	fi
+}
+
+MODE=${1:-run}
+echo "MODE: $MODE"
+
+if [ "$MODE" = "trace" ]; then
+	TRACE="ON"
+else
+	TRACE="OFF"
+fi
+
+sh build.sh $TRACE
 
 cd build
 
@@ -6,8 +33,10 @@ echo ""
 echo "TESTSTLIB:"
 echo "---------"
 cd teststlib
-./test_bsearch
-./test_radixtree
+
+run_test $MODE ./test_bsearch
+run_test $MODE ./test_radixtree
+
 cd ..
 
 echo ""
@@ -15,12 +44,17 @@ echo "TESTPARS:"
 echo "--------"
 cd testpars
 
-./test_lexer
-./test_ebnf_parser
-./test_fsm
-./test_ast
-./testpars
+run_test $MODE ./test_lexer
+run_test $MODE ./test_ebnf_parser
+run_test $MODE ./test_fsm
+run_test $MODE ./test_ast
+run_test $MODE ./testpars
 
 cd ..
 
 cd ..
+
+# TODO: short version for leaks
+# TODO: add colors for output, specially test results
+# TODO: save output?
+# >> output.txt 2>&1
