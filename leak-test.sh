09278c9c27f5f4ea2ccadc057793ce9a9cb54725
Jens Mikkelsen <jbmikk@gmail.com>
2016-01-23 12:38:41 -0300

    Add leak test script

---
+++ leak-test.sh
@@ -0,0 +1,34 @@
+sh build.sh OFF
+
+cd build
+
+VALOPTIONS = "--suppressions=../../suppressions.supp --leak-check=full --show-leak-kinds=all --track-origins=yes"
+
+echo ""
+echo "TESTSTLIB:"
+echo "---------"
+cd teststlib
+
+valgrind $VALOPTIONS ./test_bsearch
+valgrind $VALOPTIONS ./test_radixtree
+
+cd ..
+
+echo ""
+echo "TESTPARS:"
+echo "--------"
+cd testpars
+
+valgrind $VALOPTIONS ./test_lexer
+valgrind $VALOPTIONS ./test_ebnf_parser
+valgrind $VALOPTIONS ./test_fsm
+valgrind $VALOPTIONS ./test_ast
+valgrind $VALOPTIONS ./testpars
+
+# TODO: save output?
+# >> output.txt 2>&1
+
+cd ..
+
+cd ..
+
--- test.sh
+++ test.sh
@@ -14,15 +14,13 @@ echo ""
 echo "TESTPARS:"
 echo "--------"
 cd testpars
+
 ./test_lexer
 ./test_ebnf_parser
 ./test_fsm
 ./test_ast
 ./testpars
-cd ..
 
 cd ..
 
-# test with valgrind 
-# valgrind -v --suppressions=suppressions.supp --leak-check=full --show-leak-kinds=all build/teststlib/test_bsearch >> output.txt 2>&1
-# valgrind -v --suppressions=suppressions.supp --leak-check=full --show-leak-kinds=all build/teststlib/test_radixtree >> output.txt 2>&1
+cd ..
