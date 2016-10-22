7da2b3eb5c5497c95f3bb38d527745d20d3a0f6c
Jens Mikkelsen <jbmikk@gmail.com>
2016-10-22 18:59:49 -0300

    Cleanup fsm cursor header

--- src/fsmcursor.h
+++ src/fsmcursor.h
@@ -16,17 +16,25 @@ typedef struct _FsmCursor {
 } FsmCursor;
 
 void fsm_cursor_init(FsmCursor *cur, Fsm *fsm);
-Action *fsm_cursor_set_start(FsmCursor *cur, unsigned char *name, int length);
+void fsm_cursor_dispose(FsmCursor *cur);
+
+
+// Rule declaration functions
 
 void fsm_cursor_define(FsmCursor *cur, unsigned char *name, int length);
+void fsm_cursor_end(FsmCursor *cur);
 void fsm_cursor_group_start(FsmCursor *cur);
 void fsm_cursor_group_end(FsmCursor *cur);
 void fsm_cursor_loop_group_start(FsmCursor *cur);
 void fsm_cursor_loop_group_end(FsmCursor *cur);
 void fsm_cursor_or(FsmCursor *cur);
-void fsm_cursor_end(FsmCursor *cur);
-
+void fsm_cursor_terminal(FsmCursor *cur, int symbol);
 void fsm_cursor_nonterminal(FsmCursor *cur, unsigned char *name, int length);
+void fsm_cursor_done(FsmCursor *cur, int eof_symbol);
+
+
+// Internal functions?
+
 void fsm_cursor_push(FsmCursor *cur);
 void fsm_cursor_pop(FsmCursor *cur);
 void fsm_cursor_pop_discard(FsmCursor *cur);
@@ -35,14 +43,12 @@ void fsm_cursor_push_continuation(FsmCursor *cur);
 void fsm_cursor_push_new_continuation(FsmCursor *cur);
 State *fsm_cursor_pop_continuation(FsmCursor *cur);
 void fsm_cursor_join_continuation(FsmCursor *cur);
-void fsm_cursor_dispose(FsmCursor *cur);
 
-void fsm_cursor_done(FsmCursor *cur, int eof_symbol);
-void fsm_cursor_terminal(FsmCursor *cur, int symbol);
 void fsm_cursor_add_context_shift(FsmCursor *cur, int symbol);
 void fsm_cursor_add_first_set(FsmCursor *cur, State *state);
 void fsm_cursor_add_reduce(FsmCursor *cur, int symbol, int reduction);
 void fsm_cursor_add_empty(FsmCursor *cur);
-FsmCursor *fsm_set_start(Fsm *fsm, unsigned char *name, int length);
+
+Action *fsm_cursor_set_start(FsmCursor *cur, unsigned char *name, int length);
 
 #endif //FSMCURSOR_H
