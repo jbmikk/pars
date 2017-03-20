8aea98dac8ae5266c1fd95d2762ba1510f4150b2
Jens Mikkelsen <jmikkelsen@frba.utn.edu.ar>
2017-03-19 22:10:58 -0300

    Rename ast cursor variables in ebnf parser

--- src/ebnf_parser.c
+++ src/ebnf_parser.c
@@ -178,59 +178,59 @@ static int parse_utf8(char *array, unsigned int size)
 	return code;
 }
 
-void ebnf_build_character_set(FsmBuilder *builder, AstCursor *a_cur)
+void ebnf_build_character_set(FsmBuilder *builder, AstCursor *cur)
 {
 	char *string;
 	int length;
 	int first = 1;
 
-	int E_CHARACTER_PRIMARY = ast_get_symbol(a_cur, nzs("character_primary"));
-	int E_CHARACTER = ast_get_symbol(a_cur, nzs("character"));
+	int E_CHARACTER_PRIMARY = ast_get_symbol(cur, nzs("character_primary"));
+	int E_CHARACTER = ast_get_symbol(cur, nzs("character"));
 
-	ast_cursor_depth_next_symbol(a_cur, E_CHARACTER_PRIMARY);
+	ast_cursor_depth_next_symbol(cur, E_CHARACTER_PRIMARY);
 	do {
-		ast_cursor_push(a_cur);
+		ast_cursor_push(cur);
 
 		if(first) {
 			first = 0;
 		} else {
 			fsm_builder_or(builder);
 		}
-		ast_cursor_depth_next_symbol(a_cur, E_CHARACTER);
+		ast_cursor_depth_next_symbol(cur, E_CHARACTER);
 
-		ast_cursor_get_string(a_cur, &string, &length);
+		ast_cursor_get_string(cur, &string, &length);
 		int start = parse_utf8(string, length);
 
-		if(ast_cursor_next_sibling_symbol(a_cur, E_CHARACTER)) {
-			ast_cursor_get_string(a_cur, &string, &length);
+		if(ast_cursor_next_sibling_symbol(cur, E_CHARACTER)) {
+			ast_cursor_get_string(cur, &string, &length);
 			int end = parse_utf8(string, length);
 			//TODO: Verify start < end
 			fsm_builder_terminal_range(builder, start, end);
 		} else {
 			fsm_builder_terminal(builder, start);
 		}
-		ast_cursor_pop(a_cur);
+		ast_cursor_pop(cur);
 
-	} while(ast_cursor_next_sibling_symbol(a_cur, E_CHARACTER_PRIMARY));
+	} while(ast_cursor_next_sibling_symbol(cur, E_CHARACTER_PRIMARY));
 }
 
-void ebnf_build_definitions_list(FsmBuilder *builder, AstCursor *a_cur);
+void ebnf_build_definitions_list(FsmBuilder *builder, AstCursor *cur);
 
-void ebnf_build_syntactic_primary(FsmBuilder *builder, AstCursor *a_cur)
+void ebnf_build_syntactic_primary(FsmBuilder *builder, AstCursor *cur)
 {
-	AstNode *node = ast_cursor_depth_next(a_cur);
+	AstNode *node = ast_cursor_depth_next(cur);
 	char *string;
 	int length, i;
 	
 	int E_CHARACTER_SET;
-	int E_DEFINITIONS_LIST = ast_get_symbol(a_cur, nzs("definitions_list"));
+	int E_DEFINITIONS_LIST = ast_get_symbol(cur, nzs("definitions_list"));
 	switch(node->symbol) {
 	case E_META_IDENTIFIER:
-		ast_cursor_get_string(a_cur, &string, &length);
+		ast_cursor_get_string(cur, &string, &length);
 		fsm_builder_nonterminal(builder, string, length);
 		break;
 	case E_TERMINAL_STRING:
-		ast_cursor_get_string(a_cur, &string, &length);
+		ast_cursor_get_string(cur, &string, &length);
 		for(i = 1; i < length-1; i++) {
 			//TODO: literal strings should be tokenized into simbols (utf8)
 			fsm_builder_terminal(builder, string[i]);
@@ -241,28 +241,28 @@ void ebnf_build_syntactic_primary(FsmBuilder *builder, AstCursor *a_cur)
 		log_warn("Special sequence is not defined");
 		break;
 	case '(':
-		ast_cursor_depth_next_symbol(a_cur, E_DEFINITIONS_LIST);
+		ast_cursor_depth_next_symbol(cur, E_DEFINITIONS_LIST);
 		fsm_builder_group_start(builder);
-		ebnf_build_definitions_list(builder, a_cur);
+		ebnf_build_definitions_list(builder, cur);
 		fsm_builder_group_end(builder);
 		break;
 	case '{':
-		ast_cursor_depth_next_symbol(a_cur, E_DEFINITIONS_LIST);
+		ast_cursor_depth_next_symbol(cur, E_DEFINITIONS_LIST);
 		fsm_builder_loop_group_start(builder);
-		ebnf_build_definitions_list(builder, a_cur);
+		ebnf_build_definitions_list(builder, cur);
 		fsm_builder_loop_group_end(builder);
 		break;
 	case '[':
-		ast_cursor_depth_next_symbol(a_cur, E_DEFINITIONS_LIST);
+		ast_cursor_depth_next_symbol(cur, E_DEFINITIONS_LIST);
 		fsm_builder_option_group_start(builder);
-		ebnf_build_definitions_list(builder, a_cur);
+		ebnf_build_definitions_list(builder, cur);
 		fsm_builder_option_group_end(builder);
 		break;
 	default:
-		E_CHARACTER_SET = ast_get_symbol(a_cur, nzs("character_set"));
+		E_CHARACTER_SET = ast_get_symbol(cur, nzs("character_set"));
 		if(node->symbol == E_CHARACTER_SET) {
 			fsm_builder_group_start(builder);
-			ebnf_build_character_set(builder, a_cur);
+			ebnf_build_character_set(builder, cur);
 			fsm_builder_group_end(builder);
 		} else {
 			//TODO:sentinel??
@@ -270,65 +270,65 @@ void ebnf_build_syntactic_primary(FsmBuilder *builder, AstCursor *a_cur)
 	}
 }
 
-void ebnf_build_syntactic_factor(FsmBuilder *builder, AstCursor *a_cur)
+void ebnf_build_syntactic_factor(FsmBuilder *builder, AstCursor *cur)
 {
-	int E_SYNTACTIC_PRIMARY = ast_get_symbol(a_cur, nzs("syntactic_primary"));
+	int E_SYNTACTIC_PRIMARY = ast_get_symbol(cur, nzs("syntactic_primary"));
 
 	/*
 	 * TODO: Can't use depth_next methods to probe optional children.
 	 * It may jump to its grandchildren.
-	if(ast_cursor_depth_next_symbol(a_cur, E_INTEGER)) {
+	if(ast_cursor_depth_next_symbol(cur, E_INTEGER)) {
 		log_warn("Syntactic factors are not supported");
 	}
 	*/
 
-	ast_cursor_depth_next_symbol(a_cur, E_SYNTACTIC_PRIMARY);
-	ebnf_build_syntactic_primary(builder, a_cur);
+	ast_cursor_depth_next_symbol(cur, E_SYNTACTIC_PRIMARY);
+	ebnf_build_syntactic_primary(builder, cur);
 }
 
-void ebnf_build_syntactic_term(FsmBuilder *builder, AstCursor *a_cur)
+void ebnf_build_syntactic_term(FsmBuilder *builder, AstCursor *cur)
 {
-	int E_SYNTACTIC_FACTOR = ast_get_symbol(a_cur, nzs("syntactic_factor"));
-	int E_SYNTACTIC_EXCEPTION = ast_get_symbol(a_cur, nzs("syntactic_exception"));
+	int E_SYNTACTIC_FACTOR = ast_get_symbol(cur, nzs("syntactic_factor"));
+	int E_SYNTACTIC_EXCEPTION = ast_get_symbol(cur, nzs("syntactic_exception"));
 
-	ast_cursor_depth_next_symbol(a_cur, E_SYNTACTIC_FACTOR);
-	ast_cursor_push(a_cur);
-	ebnf_build_syntactic_factor(builder, a_cur);
-	ast_cursor_pop(a_cur);
-	if(ast_cursor_next_sibling_symbol(a_cur, E_SYNTACTIC_EXCEPTION)) {
+	ast_cursor_depth_next_symbol(cur, E_SYNTACTIC_FACTOR);
+	ast_cursor_push(cur);
+	ebnf_build_syntactic_factor(builder, cur);
+	ast_cursor_pop(cur);
+	if(ast_cursor_next_sibling_symbol(cur, E_SYNTACTIC_EXCEPTION)) {
 		log_warn("Syntactic exceptions are not supported");
 	}
 }
 
-void ebnf_build_single_definition(FsmBuilder *builder, AstCursor *a_cur)
+void ebnf_build_single_definition(FsmBuilder *builder, AstCursor *cur)
 {
-	int E_SYNTACTIC_TERM = ast_get_symbol(a_cur, nzs("syntactic_term"));
-	ast_cursor_depth_next_symbol(a_cur, E_SYNTACTIC_TERM);
+	int E_SYNTACTIC_TERM = ast_get_symbol(cur, nzs("syntactic_term"));
+	ast_cursor_depth_next_symbol(cur, E_SYNTACTIC_TERM);
 	do {
-		ast_cursor_push(a_cur);
-		ebnf_build_syntactic_term(builder, a_cur);
-		ast_cursor_pop(a_cur);
-	} while(ast_cursor_next_sibling_symbol(a_cur, E_SYNTACTIC_TERM));
+		ast_cursor_push(cur);
+		ebnf_build_syntactic_term(builder, cur);
+		ast_cursor_pop(cur);
+	} while(ast_cursor_next_sibling_symbol(cur, E_SYNTACTIC_TERM));
 }
 
-void ebnf_build_definitions_list(FsmBuilder *builder, AstCursor *a_cur)
+void ebnf_build_definitions_list(FsmBuilder *builder, AstCursor *cur)
 {
 	int first = 1;
-	int E_SINGLE_DEFINITION = ast_get_symbol(a_cur, nzs("single_definition"));
-	ast_cursor_depth_next_symbol(a_cur, E_SINGLE_DEFINITION);
+	int E_SINGLE_DEFINITION = ast_get_symbol(cur, nzs("single_definition"));
+	ast_cursor_depth_next_symbol(cur, E_SINGLE_DEFINITION);
 	do {
-		ast_cursor_push(a_cur);
+		ast_cursor_push(cur);
 		if(first) {
 			first = 0;
 		} else {
 			fsm_builder_or(builder);
 		}
-		ebnf_build_single_definition(builder, a_cur);
-		ast_cursor_pop(a_cur);
-	} while(ast_cursor_next_sibling_symbol(a_cur, E_SINGLE_DEFINITION));
+		ebnf_build_single_definition(builder, cur);
+		ast_cursor_pop(cur);
+	} while(ast_cursor_next_sibling_symbol(cur, E_SINGLE_DEFINITION));
 }
 
-void ebnf_build_syntax_rule(FsmBuilder *builder, AstCursor *a_cur)
+void ebnf_build_syntax_rule(FsmBuilder *builder, AstCursor *cur)
 {
 	char *string;
 	int length;
@@ -337,37 +337,37 @@ void ebnf_build_syntax_rule(FsmBuilder *builder, AstCursor *a_cur)
 	// If the ast is badly formed we could end up reading a different node
 	// than expected. This situations should be handled gracefully
 	// Maybe an error could be thrown or a sentinel could be placed.
-	ast_cursor_depth_next_symbol(a_cur, E_META_IDENTIFIER);
-	ast_cursor_get_string(a_cur, &string, &length);
+	ast_cursor_depth_next_symbol(cur, E_META_IDENTIFIER);
+	ast_cursor_get_string(cur, &string, &length);
 	fsm_builder_define(builder, string, length);
 
-	int E_DEFINITIONS_LIST = ast_get_symbol(a_cur, nzs("definitions_list"));
-	ast_cursor_next_sibling_symbol(a_cur, E_DEFINITIONS_LIST);
+	int E_DEFINITIONS_LIST = ast_get_symbol(cur, nzs("definitions_list"));
+	ast_cursor_next_sibling_symbol(cur, E_DEFINITIONS_LIST);
 	fsm_builder_group_start(builder);
-	ebnf_build_definitions_list(builder, a_cur);
+	ebnf_build_definitions_list(builder, cur);
 	fsm_builder_group_end(builder);
 	fsm_builder_end(builder);
 }	
 
 void ebnf_ast_to_fsm(Fsm *fsm, Ast *ast)
 {
-	AstCursor a_cur;
+	AstCursor cur;
 	FsmBuilder builder;
 
-	ast_cursor_init(&a_cur, ast);
+	ast_cursor_init(&cur, ast);
 	fsm_builder_init(&builder, fsm);
 
-	int E_SYNTAX_RULE = ast_get_symbol(&a_cur, nzs("syntax_rule"));
-	while(ast_cursor_depth_next_symbol(&a_cur, E_SYNTAX_RULE)) {
-		ast_cursor_push(&a_cur);
-		ebnf_build_syntax_rule(&builder, &a_cur);
-		ast_cursor_pop(&a_cur);
+	int E_SYNTAX_RULE = ast_get_symbol(&cur, nzs("syntax_rule"));
+	while(ast_cursor_depth_next_symbol(&cur, E_SYNTAX_RULE)) {
+		ast_cursor_push(&cur);
+		ebnf_build_syntax_rule(&builder, &cur);
+		ast_cursor_pop(&cur);
 	}
 
 	fsm_builder_done(&builder, L_EOF);
 	//ast_done?
 
 	fsm_builder_dispose(&builder);
-	ast_cursor_dispose(&a_cur);
+	ast_cursor_dispose(&cur);
 	
 }
