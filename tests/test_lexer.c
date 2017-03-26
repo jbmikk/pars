d794b5332afef43d62e9c8ee38f63e3d509368e8
Jens Mikkelsen <jmikkelsen@frba.utn.edu.ar>
2017-03-25 22:51:30 -0300

    Fix lexer tests indentation

--- tests/test_lexer.c
+++ tests/test_lexer.c
@@ -62,101 +62,101 @@ void t_teardown(){
 	input_dispose(&fix.input_integer);
 	input_dispose(&fix.input_identifier);
 	input_dispose(&fix.input_terminal_string);
-		input_dispose(&fix.input_rule_one);
-		input_dispose(&fix.input_white);
-	}
+	input_dispose(&fix.input_rule_one);
+	input_dispose(&fix.input_white);
+}
 
-	void lexer_input_next__integer_token(){
-		lexer_next(&fix.lexer_integer, &fix.token);
-		t_assert(fix.token.symbol == E_INTEGER);
-		t_assert(fix.token.index == 0);
-		t_assert(fix.token.length == strlen(I_INTEGER));
-	}
+void lexer_input_next__integer_token(){
+	lexer_next(&fix.lexer_integer, &fix.token);
+	t_assert(fix.token.symbol == E_INTEGER);
+	t_assert(fix.token.index == 0);
+	t_assert(fix.token.length == strlen(I_INTEGER));
+}
 
-	void lexer_input_next__identifier_token(){
-		lexer_next(&fix.lexer_identifier, &fix.token);
-		t_assert(fix.token.symbol == E_META_IDENTIFIER);
-		t_assert(fix.token.index == 0);
-		t_assert(fix.token.length == strlen(I_IDENTIFIER));
-	}
+void lexer_input_next__identifier_token(){
+	lexer_next(&fix.lexer_identifier, &fix.token);
+	t_assert(fix.token.symbol == E_META_IDENTIFIER);
+	t_assert(fix.token.index == 0);
+	t_assert(fix.token.length == strlen(I_IDENTIFIER));
+}
 
-	void lexer_input_next__terminal_string_token(){
-		lexer_next(&fix.lexer_terminal_string, &fix.token);
-		t_assert(fix.token.symbol == E_TERMINAL_STRING);
-		t_assert(fix.token.index == 0);
-		t_assert(fix.token.length == strlen(I_TERMINAL_STRING));
-	}
+void lexer_input_next__terminal_string_token(){
+	lexer_next(&fix.lexer_terminal_string, &fix.token);
+	t_assert(fix.token.symbol == E_TERMINAL_STRING);
+	t_assert(fix.token.index == 0);
+	t_assert(fix.token.length == strlen(I_TERMINAL_STRING));
+}
 
-	void lexer_input_next__skip_white_space(){
-		lexer_next(&fix.lexer_rule_one, &fix.token);
-		t_assert(fix.token.symbol == E_META_IDENTIFIER);
-		t_assert(fix.token.index == 0);
-		t_assert(fix.token.length == 3);
-		lexer_next(&fix.lexer_rule_one, &fix.token);
-		t_assert(fix.token.symbol == E_DEFINING_SYMBOL);
-		t_assert(fix.token.index == 4);
-		t_assert(fix.token.length == 1);
-	}
+void lexer_input_next__skip_white_space(){
+	lexer_next(&fix.lexer_rule_one, &fix.token);
+	t_assert(fix.token.symbol == E_META_IDENTIFIER);
+	t_assert(fix.token.index == 0);
+	t_assert(fix.token.length == 3);
+	lexer_next(&fix.lexer_rule_one, &fix.token);
+	t_assert(fix.token.symbol == E_DEFINING_SYMBOL);
+	t_assert(fix.token.index == 4);
+	t_assert(fix.token.length == 1);
+}
 
-	void lexer_input_next__whole_rule(){
-		lexer_next(&fix.lexer_rule_one, &fix.token);
-		t_assert(fix.token.symbol == E_META_IDENTIFIER);
-		t_assert(fix.token.index == 0);
-		t_assert(fix.token.length == 3);
-		lexer_next(&fix.lexer_rule_one, &fix.token);
-		t_assert(fix.token.symbol == E_DEFINING_SYMBOL);
-		t_assert(fix.token.index == 4);
-		t_assert(fix.token.length == 1);
-		lexer_next(&fix.lexer_rule_one, &fix.token);
-		t_assert(fix.token.symbol == E_TERMINAL_STRING);
-		t_assert(fix.token.index == 6);
-		t_assert(fix.token.length == 3);
-		lexer_next(&fix.lexer_rule_one, &fix.token);
-		t_assert(fix.token.symbol == E_CONCATENATE_SYMBOL);
-		t_assert(fix.token.index == 9);
-		t_assert(fix.token.length == 1);
-		lexer_next(&fix.lexer_rule_one, &fix.token);
-		t_assert(fix.token.symbol == E_START_GROUP_SYMBOL);
-		t_assert(fix.token.index == 10);
-		t_assert(fix.token.length == 1);
-		lexer_next(&fix.lexer_rule_one, &fix.token);
-		t_assert(fix.token.symbol == E_TERMINAL_STRING);
-		t_assert(fix.token.index == 11);
-		t_assert(fix.token.length == 3);
-		lexer_next(&fix.lexer_rule_one, &fix.token);
-		t_assert(fix.token.symbol == E_DEFINITION_SEPARATOR_SYMBOL);
-		t_assert(fix.token.index == 14);
-		t_assert(fix.token.length == 1);
-		lexer_next(&fix.lexer_rule_one, &fix.token);
-		t_assert(fix.token.symbol == E_TERMINAL_STRING);
-		t_assert(fix.token.index == 15);
-		t_assert(fix.token.length == 3);
-		lexer_next(&fix.lexer_rule_one, &fix.token);
-		t_assert(fix.token.symbol == E_END_GROUP_SYMBOL);
-		t_assert(fix.token.index == 18);
-		t_assert(fix.token.length == 1);
-		lexer_next(&fix.lexer_rule_one, &fix.token);
-		t_assert(fix.token.symbol == L_EOF);
-		t_assert(fix.token.index == 19);
-		t_assert(fix.token.length == 0);
-	}
+void lexer_input_next__whole_rule(){
+	lexer_next(&fix.lexer_rule_one, &fix.token);
+	t_assert(fix.token.symbol == E_META_IDENTIFIER);
+	t_assert(fix.token.index == 0);
+	t_assert(fix.token.length == 3);
+	lexer_next(&fix.lexer_rule_one, &fix.token);
+	t_assert(fix.token.symbol == E_DEFINING_SYMBOL);
+	t_assert(fix.token.index == 4);
+	t_assert(fix.token.length == 1);
+	lexer_next(&fix.lexer_rule_one, &fix.token);
+	t_assert(fix.token.symbol == E_TERMINAL_STRING);
+	t_assert(fix.token.index == 6);
+	t_assert(fix.token.length == 3);
+	lexer_next(&fix.lexer_rule_one, &fix.token);
+	t_assert(fix.token.symbol == E_CONCATENATE_SYMBOL);
+	t_assert(fix.token.index == 9);
+	t_assert(fix.token.length == 1);
+	lexer_next(&fix.lexer_rule_one, &fix.token);
+	t_assert(fix.token.symbol == E_START_GROUP_SYMBOL);
+	t_assert(fix.token.index == 10);
+	t_assert(fix.token.length == 1);
+	lexer_next(&fix.lexer_rule_one, &fix.token);
+	t_assert(fix.token.symbol == E_TERMINAL_STRING);
+	t_assert(fix.token.index == 11);
+	t_assert(fix.token.length == 3);
+	lexer_next(&fix.lexer_rule_one, &fix.token);
+	t_assert(fix.token.symbol == E_DEFINITION_SEPARATOR_SYMBOL);
+	t_assert(fix.token.index == 14);
+	t_assert(fix.token.length == 1);
+	lexer_next(&fix.lexer_rule_one, &fix.token);
+	t_assert(fix.token.symbol == E_TERMINAL_STRING);
+	t_assert(fix.token.index == 15);
+	t_assert(fix.token.length == 3);
+	lexer_next(&fix.lexer_rule_one, &fix.token);
+	t_assert(fix.token.symbol == E_END_GROUP_SYMBOL);
+	t_assert(fix.token.index == 18);
+	t_assert(fix.token.length == 1);
+	lexer_next(&fix.lexer_rule_one, &fix.token);
+	t_assert(fix.token.symbol == L_EOF);
+	t_assert(fix.token.index == 19);
+	t_assert(fix.token.length == 0);
+}
 
-	void lexer_input_next__white_token(){
-		lexer_next(&fix.lexer_white, &fix.token);
-		t_assert(fix.token.symbol == E_META_IDENTIFIER);
-		t_assert(fix.token.index == 5);
-		t_assert(fix.token.length == strlen("identifier"));
-	}
+void lexer_input_next__white_token(){
+	lexer_next(&fix.lexer_white, &fix.token);
+	t_assert(fix.token.symbol == E_META_IDENTIFIER);
+	t_assert(fix.token.index == 5);
+	t_assert(fix.token.length == strlen("identifier"));
+}
 
-	void lexer_input_next__utf8_two_byte(){
-		lexer_next(&fix.lexer_utf8_two_byte, &fix.token);
-		t_assert(fix.token.symbol == 0xF1);
-		t_assert(fix.token.index == 0);
-		t_assert(fix.token.length == 2);
-	}
+void lexer_input_next__utf8_two_byte(){
+	lexer_next(&fix.lexer_utf8_two_byte, &fix.token);
+	t_assert(fix.token.symbol == 0xF1);
+	t_assert(fix.token.index == 0);
+	t_assert(fix.token.length == 2);
+}
 
-	void lexer_input_next__utf8_three_byte(){
-		lexer_next(&fix.lexer_utf8_three_byte, &fix.token);
+void lexer_input_next__utf8_three_byte(){
+	lexer_next(&fix.lexer_utf8_three_byte, &fix.token);
 	t_assert(fix.token.symbol == 0xF71);
 	t_assert(fix.token.index == 0);
 	t_assert(fix.token.length == 3);
