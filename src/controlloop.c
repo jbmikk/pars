1c3207c7fdcb1aa36c0382d6df6f0578597a7f0b
Jens Mikkelsen <jmikkelsen@frba.utn.edu.ar>
2018-05-14 21:51:38 -0300

    Add basic ast continuation follow function

--- src/controlloop.c
+++ src/controlloop.c
@@ -70,13 +70,55 @@ error:
 	return -1;
 }
 
+int ast_continuation_follow(const Continuation *cont, AstCursor *cursor, Token *token)
+{
+	int ret;
+	AstNode *node;
+
+	if(cont->error) {
+		ret = cont->error;
+		goto end;
+	}
+	switch(cont->transition.action->type) {
+	case ACTION_START:
+		node = ast_cursor_depth_next(cursor);
+		token_init(token, 0, 0, node->token.symbol);
+
+		ret = 0;
+		break;
+	case ACTION_ACCEPT:
+	case ACTION_SHIFT:
+	case ACTION_DROP:
+		if(token->symbol == L_EOF) {
+			ret = -3;
+			break;
+		}
+
+		// TODO: Should the index be part of the ast?
+		// We need the index for input back tracking. We should be 
+		// able to move the iterator back to the specific node.
+		int index = cont->transition.token.index + 1;
+		node = ast_cursor_depth_next(cursor);
+		token_init(token, index, 0, node->token.symbol);
+		ret = 0;
+		break;
+	case ACTION_ERROR:
+		ret = -1;
+	default:
+		// TODO: sentinel? should never reach this place
+		ret = -2;
+		break;
+	}
+
+end:
+	return ret;
+}
+
 int control_loop_ast(void *object, void *params)
 {
 	ParserContext *context = (ParserContext *)object;
 	Token token;
 	token_init(&token, 0, 0, 0);
-
-	unsigned int index = 0;
 
 	Symbol *t_down = symbol_table_get(&context->parser->table, "__tdown", 7);
 	Symbol *t_up = symbol_table_get(&context->parser->table, "__tup", 5);
@@ -89,43 +131,40 @@ int control_loop_ast(void *object, void *params)
 	AstCursor cursor;
 	ast_cursor_init(&cursor, context->input_ast);
 
-	AstNode *node = ast_cursor_depth_next(&cursor);
-	Continuation cont;
+	Continuation cont = { .error = 0 };
 
-	while(node) {
+	// Dummy action for initial continuation
+	Action dummy;
+	action_init(&dummy, ACTION_START, 0, NULL, 0, 0);
+
+	token_init(&cont.transition.token, 0, 0, 0);
+	cont.transition.action = &dummy;
+
+	while(!ast_continuation_follow(&cont, &cursor, &token)) {
 
 		if(cursor.offset == 1) {
 			cont = fsm_pda_loop(&context->thread, token_down, context->parser_transition);
 			check(
 				!cont.error,
-				"Parser error at node %p - DOWN", node
+				"Parser error at DOWN node"
 			);
 		} else if(cursor.offset < 0) {
 			cont = fsm_pda_loop(&context->thread, token_up, context->parser_transition);
 			check(
 				!cont.error,
-				"Parser error at node %p - UP", node
+				"Parser error at UP node"
 			);
 		}
-
-		token_init(&token, index, 0, node->token.symbol);
 
 		// TODO: no need for a full pda loop for now, a simple fsm
 		// should do.
 		cont = fsm_pda_loop(&context->thread, token, context->parser_transition);
 		check(
 			!cont.error,
-			"Parser error at node %p - "
-			"index: %i with symbol: %i, length: %i", node,
-			node->token.index, node->token.symbol, node->token.length
+			"Parser error at token "
+			"index: %i with symbol: %i, length: %i",
+			token.index, token.symbol, token.length
 		);
-
-		// TODO: Should the index be part of the ast?
-		// We need the index for input back tracking. We should be 
-		// able to move the iterator back to the specific node.
-		index++;
-
-		node = ast_cursor_depth_next(&cursor);
 	}
 	ast_cursor_dispose(&cursor);
 
