8cdfc9fdfa216eee37b751f207b61c4ff2a2e015
Jens Mikkelsen <jmikkelsen@frba.utn.edu.ar>
2018-09-24 23:11:25 -0300

    Refactor references solving

--- src/reference.c
+++ src/reference.c
@@ -4,9 +4,116 @@
 
 #include "fsmtrace.h"
 
+static int _clone_action(Reference *ref, int key, Action *action)
+{
+	int unsolved = 0;
+	//TODO: Make type for clone a parameter, do not override by
+	// default.
+
+	// When a symbol is present, assume nonterminal invocation
+	int clone_type;
+	if(ref->symbol) {
+		if (action->type == ACTION_REDUCE) {
+			// This could happen when the start state of a
+			// nonterminal is also an end state.
+			trace_op(
+				"skip",
+				ref->state,
+				action,
+				key,
+				"reduction on first-set",
+				0
+			);
+			goto end;
+		}
+		clone_type = ACTION_SHIFT;
+	} else {
+		// It could happen when merging loops in final states
+		// that action->type == ACTION_REDUCE
+		clone_type = action->type;
+	}
+
+	Action *col = state_get_transition(ref->state, key);
+
+	if(col) {
+		if(
+			col->type == action->type &&
+			col->state == action->state &&
+			col->reduction == action->reduction &&
+			col->end_symbol == action->end_symbol &&
+			col->flags == action->flags
+		) {
+			trace_op(
+				"collision",
+				ref->state,
+				action,
+				key,
+				"skip duplicate",
+				0
+			);
+			goto end;
+		}
+
+		if(col->state == NULL || action->state == NULL) {
+			trace_op(
+				"collision",
+				ref->state,
+				action,
+				key,
+				"unhandled",
+				0
+			);
+			goto end;
+		}
+
+		//Collision: redefine actions in order to disambiguate.
+		trace_op(
+			"collision",
+			ref->state,
+			col,
+			key,
+			"deambiguate state",
+			0
+		);
+
+		//TODO: should only merge if actions are identical
+		//TODO: Make this work with ranges.
+		State *merge = malloc(sizeof(State));
+		state_init(merge);
+
+		//Merge first set for the actions continuations.
+		state_add_reference(merge, NULL, col->state);
+		state_add_reference(merge, NULL, action->state);
+
+		//Create unified action pointing to merged state.
+		action_init(col, clone_type, col->reduction, merge, col->flags, col->end_symbol);
+
+		//TODO: Is it necessary to return the unsolved count?
+		// Shouldn't it be enough to have further references pushed?
+		unsolved = 1;
+	} else {
+
+		//No collision detected, clone the action an add it.
+		Action clone;
+		action_init(&clone, clone_type, action->reduction, action->state, action->flags, action->end_symbol);
+
+		trace_op(
+			"add",
+			ref->state,
+			&clone,
+			key,
+			"first-set",
+			0
+		);
+
+		state_add_action(ref->state, key, &clone);
+	}
+end:
+	return unsolved;
+}
+
 void reference_solve_first_set(Reference *ref, int *unsolved)
 {
-	Action *action;
 	BMapCursorAction cursor;
 	BMapEntryAction *entry;
 
@@ -35,106 +142,7 @@ void reference_solve_first_set(Reference *ref, int *unsolved)
 	bmap_cursor_action_init(&cursor, &(ref->to_state->actions));
 	while(bmap_cursor_action_next(&cursor)) {
 		entry = bmap_cursor_action_current(&cursor);
-		action = &entry->action;
-		//TODO: Make type for clone a parameter, do not override by
-		// default.
-
-		// When a symbol is present, assume nonterminal invocation
-		int clone_type;
-		if(ref->symbol) {
-			if (action->type == ACTION_REDUCE) {
-				// This could happen when the start state of a
-				// nonterminal is also an end state.
-				trace_op(
-					"skip",
-					ref->state,
-					action,
-					entry->key,
-					"reduction on first-set",
-					0
-				);
-				 continue;
-			}
-			clone_type = ACTION_SHIFT;
-		} else {
-			// It could happen when merging loops in final states
-			// that action->type == ACTION_REDUCE
-			clone_type = action->type;
-		}
-
-		Action *col = state_get_transition(ref->state, entry->key);
-
-		if(col) {
-			if(
-				col->type == action->type &&
-				col->state == action->state &&
-				col->reduction == action->reduction &&
-				col->end_symbol == action->end_symbol &&
-				col->flags == action->flags
-			) {
-				trace_op(
-					"collision",
-					ref->state,
-					action,
-					entry->key,
-					"skip duplicate",
-					0
-				);
-				continue;
-			}
-
-			if(col->state == NULL || action->state == NULL) {
-				trace_op(
-					"collision",
-					ref->state,
-					action,
-					entry->key,
-					"unhandled",
-					0
-				);
-				continue;
-			}
-
-			//Collision: redefine actions in order to disambiguate.
-			trace_op(
-				"collision",
-				ref->state,
-				col,
-				entry->key,
-				"deambiguate state",
-				0
-			);
-
-			//TODO: should only merge if actions are identical
-			//TODO: Make this work with ranges.
-			State *merge = malloc(sizeof(State));
-			state_init(merge);
-
-			//Merge first set for the actions continuations.
-			state_add_reference(merge, NULL, col->state);
-			state_add_reference(merge, NULL, action->state);
-
-			//Create unified action pointing to merged state.
-			action_init(col, clone_type, col->reduction, merge, col->flags, col->end_symbol);
-
-			*unsolved = 1;
-		} else {
-
-			//No collision detected, clone the action an add it.
-			Action clone;
-			action_init(&clone, clone_type, action->reduction, action->state, action->flags, action->end_symbol);
-
-			trace_op(
-				"add",
-				ref->state,
-				&clone,
-				entry->key,
-				"first-set",
-				0
-			);
-
-			state_add_action(ref->state, entry->key, &clone);
-		}
+		*unsolved += _clone_action(ref, entry->key, &entry->action);
 	}
 	bmap_cursor_action_dispose(&cursor);
 
