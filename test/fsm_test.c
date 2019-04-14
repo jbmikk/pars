8911b302686a2ec943f3f9de1e696429220d8af1
Jens Mikkelsen <jmikkelsen@frba.utn.edu.ar>
2019-04-14 12:58:04 -0300

    Add simple fsm repetition test

--- test/fsm_test.c
+++ test/fsm_test.c
@@ -449,6 +449,47 @@ void fsm_thread_match__first_set_collision(){
 	fsm_thread_dispose(&thread);
 }
 
+void fsm_thread_match__simple_repetition(){
+	FsmBuilder builder;
+	Transition tran;
+
+	fsm_builder_init(&builder, &fix.fsm, REF_STRATEGY_SPLIT);
+
+	fsm_builder_set_mode(&builder, nzs(".default"));
+
+	fsm_builder_define(&builder, nzs("test"));
+	fsm_builder_terminal(&builder, 'a');
+	fsm_builder_loop_group_start(&builder);
+	fsm_builder_terminal(&builder, 'b');
+	fsm_builder_loop_group_end(&builder);
+	fsm_builder_end(&builder);
+
+	fsm_builder_lexer_done(&builder, '\0');
+
+	fsm_builder_dispose(&builder);
+
+	int test = fsm_get_symbol_id(&fix.fsm, nzs("test"));
+
+	FsmThread thread;
+	fsm_thread_init(&thread, &fix.fsm, (Listener) { .function = NULL });
+	fsm_thread_start(&thread);
+
+	MATCH_START(thread, 'a');
+	MATCH_ACCEPT_WITH(thread, '\0', test);
+
+	MATCH_START(thread, 'a');
+	MATCH_DROP(thread, 'b');
+	MATCH_DROP(thread, 'b');
+	MATCH_DROP(thread, 'b');
+	MATCH_ACCEPT_WITH(thread, '\0', test);
+
+	MATCH_START(thread, 'a');
+	MATCH_DROP(thread, 'b');
+	MATCH_ACCEPT_WITH(thread, '\0', test);
+
+	fsm_thread_dispose(&thread);
+}
+
 void fsm_thread_match__repetition(){
 	FsmBuilder builder;
 	Transition tran;
@@ -766,6 +807,7 @@ int main(int argc, char** argv){
 	t_test(fsm_thread_match__reduce_shift);
 	t_test(fsm_thread_match__reduce_handler);
 	t_test(fsm_thread_match__first_set_collision);
+	t_test(fsm_thread_match__simple_repetition);
 	t_test(fsm_thread_match__repetition);
 	t_test(fsm_thread_match__any);
 	t_test(fsm_thread_match__copy);
